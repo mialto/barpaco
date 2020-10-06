7ca72be8fca1bab309281e631824ad2420515c6a
mialto <miguel.a.torralba@gmail.com>
2020-10-06 11:55:44 +0200

    insertar ckeditor

--- src/AppBundle/Controller/GestionTapasController.php
+++ src/AppBundle/Controller/GestionTapasController.php
@@ -23,15 +23,27 @@ class GestionTapasController extends Controller
      */
     public function nuevaTapaAction(Request $request)
     {
-        if(!is_null($request)){
-            $datos=$request->request->all();
-            var_dump($datos);
-        }
+    
         $tapa = new Tapa();
         //construyendo el formulario
         $form = $this->createForm(TapaType::class, $tapa);
-        
-        // replace this example code with whatever you need
+        //Recogemos la informacion del submit
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $tapa = $form->getData();
+
+            $tapa->setFoto("");
+            $tapa->setFechaCreacion(new \DateTime());
+
+            //Almacenar nueva tapa
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($tapa);
+            $em->flush();
+    
+            //redirige a la url de la tapa que acabamos de crear
+            return $this->redirectToRoute('tapa', array('id' => $tapa->getId()));
+        }
+        // replace this example code with wmhatever you need
         return $this->render('gestionTapas/nuevaTapa.html.twig', array('form'=>$form->createView()));
     }
 }
--- src/AppBundle/Form/TapaType.php
+++ src/AppBundle/Form/TapaType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 
 use AppBundle\Entity\Tapa;
 
@@ -17,8 +18,9 @@ class TapaType extends AbstractType
     {
         $builder
             ->add('nombre', TextType::class)
-            ->add('descripcion', TextAreaType::class)
+            ->add('descripcion', CKEditorType::class)
             ->add('ingredientes', TextareaType::class)
+            ->add('top')
             ->add('save', SubmitType::class, array('label'=>'Crear tapa'))
         ;
     }
