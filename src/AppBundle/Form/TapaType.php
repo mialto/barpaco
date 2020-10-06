946a2f5a85dad6e6a7701f22cac86a0b88416a36
mialto <miguel.a.torralba@gmail.com>
2020-10-06 17:22:17 +0200

    poner el fileupload

--- src/AppBundle/Form/TapaType.php
+++ src/AppBundle/Form/TapaType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Ivory\CKEditorBundle\Form\Type\CKEditorType;
 
 use AppBundle\Entity\Tapa;
@@ -20,6 +21,7 @@ class TapaType extends AbstractType
             ->add('nombre', TextType::class)
             ->add('descripcion', CKEditorType::class)
             ->add('ingredientes', TextareaType::class)
+            ->add('foto', FileType::class, array('attr'=> array('onchange'=>'onChange(event)')))
             ->add('top')
             ->add('save', SubmitType::class, array('label'=>'Crear tapa'))
         ;
